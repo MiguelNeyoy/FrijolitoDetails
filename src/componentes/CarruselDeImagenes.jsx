069f1168729ae23adde4d0e9df3213b7126fe3a4
MiguelNeyoy <miguelneyoycn@gmail.com>
2025-07-07 18:10:09 -0700

    carruselImg
    Se implemento el carrucel de imagenes al final de la trivia

---
+++ src/componentes/CarruselDeImagenes.jsx
@@ -0,0 +1,40 @@
+import "../styles/Carrusel.css";
+import { useState } from "react";
+import arrowLeft from "../assets/arrow-left-icon.svg";
+import arrowRight from "../assets/arrow-right-icon.svg";
+import { motion, AnimatePresence } from "motion/react";
+export default function Carrusel({ imagenes, respuestaTrivia }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === imagenes.length ? 0 : prevIndex + 1
+    );
+  };
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? imagenes.length - 1 : prevIndex - 1
+    );
+  };
+  return (
+    <div className="carousel">
+      <div className="slide_direction">
+        <div className="left" onClick={handlePrevious}>
+          <img
+            src={arrowLeft}
+            alt="icon flecha izquierda"
+            className="arrowLeft"
+          />
+        </div>
+        <img
+          key={currentIndex}
+          src={imagenes[currentIndex]}
+          className="imgCarrusel"
+        />
+        <div className="right" onClick={handleNext}>
+          <img src={arrowRight} alt="flecha derecha" className="arrowRight" />
+        </div>
+      </div>
+      <div className="respuestas">{respuestaTrivia}</div>
+    </div>
+  );
+}
--- src/page/Trivia.jsx
+++ src/page/Trivia.jsx
@@ -1,7 +1,13 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import PreguntaTrivia from "../componentes/PreguntasTrivia.jsx";
+import Carrusel from "../componentes/CarruselDeImagenes.jsx";
 import "../styles/trivia.css";
+import imgCarrusel1 from "../assets/imgCarruselUno.jpg";
+import imgCarrusel2 from "../assets/imgCarruselDos.jpg";
+import imgCarrusel3 from "../assets/imgCarruselTres.jpg";
+import imgCarrusel4 from "../assets/imgCarruselCuatro.jpg";
+import imgCarrusel5 from "../assets/imgCarruselCinco.jpg";
 
 function Trivia() {
   const preguntas = [
@@ -31,16 +37,25 @@ function Trivia() {
   ];
 
   const [indice, setIndice] = useState(0);
-  const navigate = useNavigate();
-
+  const [showCarrusel, setShowCarrusel] = useState(false);
+  const [restulTrivia, setResultTrivia] = useState([]);
   const avanzar = (respuesta) => {
+    setResultTrivia((prev) => [...prev, respuesta]);
     console.log("Respuesta : ", respuesta);
     if (indice + 1 < preguntas.length) {
       setTimeout(() => setIndice(indice + 1), 1000); // Espera 1 seg y muestra la siguiente
     } else {
-      navigate("/");
+      setShowCarrusel(true);
     }
   };
+
+  const imagenes = [
+    imgCarrusel1,
+    imgCarrusel2,
+    imgCarrusel3,
+    imgCarrusel4,
+    imgCarrusel5,
+  ];
 
   return (
     <div className="main">
@@ -52,6 +67,23 @@ function Trivia() {
         opcion={preguntas[indice].opcion}
         onRespons={avanzar}
       />
+      {showCarrusel && (
+        <div onClick={() => setShowCarrusel(false)} className="modalCarrusel">
+          <div
+            className="contentModalCarrusel"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button
+              className="btnModalCloseCarrusel"
+              onClick={() => setShowCarrusel(false)}
+            >
+              {" "}
+              Cerrar
+            </button>
+            <Carrusel imagenes={imagenes} respuestaTrivia={restulTrivia} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
