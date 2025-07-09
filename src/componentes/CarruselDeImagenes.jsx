43c68c588214638126f96e692aa2e61449728b28
MiguelNeyoy <miguelneyoycn@gmail.com>
2025-07-08 17:17:23 -0700

    Se termino el carrucel
    Se puede mejorar agrgando animaciones de scroll, refactorizando el codigo y otras cosas, quedara pendiente

--- src/componentes/CarruselDeImagenes.jsx
+++ src/componentes/CarruselDeImagenes.jsx
@@ -5,6 +5,7 @@ import arrowRight from "../assets/arrow-right-icon.svg";
 import { motion, AnimatePresence } from "motion/react";
 export default function Carrusel({ imagenes, respuestaTrivia }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [corretAnser, setCorrectAnser] = useState([]);
   const handleNext = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex + 1 === imagenes.length ? 0 : prevIndex + 1
@@ -25,16 +26,25 @@ export default function Carrusel({ imagenes, respuestaTrivia }) {
             className="arrowLeft"
           />
         </div>
+
         <img
           key={currentIndex}
           src={imagenes[currentIndex]}
           className="imgCarrusel"
         />
-        <div className="right" onClick={handleNext}>
-          <img src={arrowRight} alt="flecha derecha" className="arrowRight" />
+
+        <div className="respuestas">
+          {respuestaTrivia.map((r, i) => (
+            <div key={i}>
+              {" "}
+              {`${i + 1}._`} {r}
+            </div>
+          ))}
+          <div className="right" onClick={handleNext}>
+            <img src={arrowRight} alt="flecha derecha" className="arrowRight" />
+          </div>
         </div>
       </div>
-      <div className="respuestas">{respuestaTrivia}</div>
     </div>
   );
 }
