91b880e854f421c1660e3f8bbb9b99d366608612
MiguelNeyoy <miguelneyoycn@gmail.com>
2025-09-12 14:45:51 -0700

    Estilos y estructura del detalle
    Se realizo el html y se avanzo un 50% de los estilos

--- src/page/CreadorPromesa.jsx
+++ src/page/CreadorPromesa.jsx
@@ -1,12 +1,24 @@
+import { Link } from "react-router-dom";
+import homeIcon from "../assets/home.svg";
+import "../styles/MainPromesa.css";
 export default function CreadorPromesa() {
   return (
-    <aside>
-      <nav></nav>
-      <p>Creador de promesas</p>
-      <section></section>
-      <form>
+    <aside className="main-promesa">
+      <nav className="nav-promesa">
+        <Link to={"/"} className="linkHomePromesa">
+          <img src={homeIcon} title="home" className="homeIcon" />
+        </Link>
+      </nav>
+      <p className="parrafo-promesa">Creador de promesas</p>
+      <p className="parrafo-promesa">Para "Nombre", mi todo </p>
+      <section className="container-promesa">
+        <p>Aqui va colocada la Promesa</p>
+        <img src="" alt="Imagen rotativa entre cada promesa " />
+        <button>Siguente Promesa</button>
+      </section>
+      <form className="container-form">
         <input type="text" />
-        <button type="submit"></button>
+        <button type="button">Añadir Promesa</button>
       </form>
     </aside>
   );
